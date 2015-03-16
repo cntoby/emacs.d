adee5370d0a2eb16f3df4826be806973e5fb6998
Toby <cntoby@gmail.com>
2015-03-16 18:40:46 +0800

    my emacs.d init

---
+++ init.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "~/.emacs.d/modes")
+(autoload 'markdown-mode "markdown-mode" "Major mode for edting Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
