2dab354a806e220a86b82ebf9f75e2d29338be51
Toby <cntoby@gmail.com>
2015-03-17 17:39:49 +0800

    fixed pandoc parameters

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(markdown-command
-   "pandoc -f markdown -t html -s —mathjax —highlight-style pygments"))
+   "pandoc -f markdown -t html -s -—mathjax -—highlight-style pygments"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
