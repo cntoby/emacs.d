40f723b4eff03468d6022d6e37825d96a0331824
Toby <cntoby@gmail.com>
2015-11-10 18:54:15 +0800

    add exec-path-from-shell call

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -2,8 +2,7 @@
   (setenv "GOPATH" (expand-file-name "~/Devel/goprojects/"))
   (require 'go-mode-autoloads)
   (add-hook 'go-mode-hook (lambda ()
-			    (local-set-key (kbd "M-.") #'godef-jump)
-			    ))
+			    (local-set-key (kbd "M-.") #'godef-jump)))
   (add-hook 'before-save-hook #'gofmt-before-save) ; format code before save
 
   (when (package-installed-p 'go-eldoc)
@@ -13,11 +12,8 @@
 			:underline t :foreground "green"
 			:weight 'bold))
   (when (package-installed-p 'go-snippets)
-    (require 'go-snippets)
-    )
+    (require 'go-snippets))
   (when (memq window-system '(mac ns))
-    (setq go-eldoc-gocode (expand-file-name "~/Devel/goprojects/bin/gocode"))
-    ; (exec-path-from-shell-initialize)
-    ; (exec-path-from-shell-copy-env "GOPATH")
-    ))
+    (when (featurep 'exec-path-from-shell-initialize)
+      (exec-path-from-shell-copy-env "GOPATH"))))
 (provide 'init-go)
