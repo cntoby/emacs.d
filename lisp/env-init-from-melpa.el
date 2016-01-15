bc26a5ea4e320a9293531e52707127e1c28d36a3
Toby <cntoby@gmail.com>
2016-01-16 01:46:30 +0800

    add some plugin for emacs

--- lisp/env-init-from-melpa.el
+++ lisp/env-init-from-melpa.el
@@ -1,27 +1,58 @@
 (setq env-init--package-list
       '(company
 	go-mode
+	go-snippets
+	;;go-eldoc
 	haskell-mode
+	haskell-snippets
+	;;ghc
 	helm
+	helm-core
+	;;helm-circe
+	helm-projectile
 	markdown-mode
-	php-mode
+	jekyll-modes
 	projectile
+	projectile-codesearch
+	pkg-info
 	;;smex
+	;;fish-mode
+	;;idomenu
+	;;kanban
+	;;circe
+	;;codesearch
+	;;company-go
+	;;company-web
+	;;epresent
+	;;fish-mode
+	org
+	org-journal
+	org-pandoc
+	;;org-tree-slide
+	php-mode
+	lua-mode
+	js2-mode
+	typescript-mode
+	web-mode
+	yaml-mode
+	websocket
+	;;xcscope
 	sublime-themes
 	material-theme
 	window-numbering
 	yasnippet
-	go-snippets
-	haskell-snippets
+	company
 	ycmd
 	company-ycmd
 	flycheck
 	flycheck-ycmd
 	erlang
+	python-environment
 	python-info
 	python3-info
 	man-commands
 	magit
+	magit-popup
 	exec-path-from-shell
 	powerline))
 
