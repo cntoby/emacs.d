2a1b5eeb8856e449c4568abcd6ee97786d1a960c
Toby <cntoby@gmail.com>
2016-04-28 17:42:58 +0800

    add rust autocomplete support with company-racer

---
+++ lisp/init-rust.el
@@ -0,0 +1,13 @@
+(when (package-installed-p 'rust-mode)
+  (when (package-installed-p 'company-racer)
+    (unless (getenv "RUST_SRC_PATH")
+      (cond ((string-equal system-type "darwin")
+	     (setenv "RUST_SRC_PATH" (expand-file-name "~/Devel/src/rust/src")))
+	    ((string-equal system-type "gnu/linux")
+	     (setenv "RUST_SRC_PATH" (expand-file-name "~/src/rust/src")))))
+    (require 'company-racer)
+    (with-eval-after-load 'company
+      (add-to-list 'company-backends 'company-racer)))
+  (when (package-installed-p 'rustfmt)
+    (add-hook 'rust-mode-hook #'rustfmt-enable-on-save)))
+(provide 'init-rust)
