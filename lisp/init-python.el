33d56d4cbe39d903201e64e2927caddbbd95882c
Toby <cntoby@gmail.com>
2016-05-06 18:32:54 +0800

    add shortcuts to jedi:goto-definition and show-doc

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -4,6 +4,8 @@
 	      (require 'jedi-core)
 	      (setq jedi:complete-on-dot t)
 	      (setq jedi:use-shortcuts t)
-	      (add-to-list 'company-backends 'company-jedi))))
+	      (add-to-list 'company-backends 'company-jedi)
+	      (local-set-key (kbd "M-.") #'jedi:goto-definition)
+	      (local-set-key (kbd "C-c C-d") #'jedi:show-doc))))
 ; (add-hook 'python-mode-hook 'jedi:setup) ; execute jedi:install-server when first running
 (provide 'init-python)
