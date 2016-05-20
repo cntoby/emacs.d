8e4489db5b1a5c1d293909e22c7773cb54e1c951
Toby <cntoby@gmail.com>
2016-05-20 14:32:33 +0800

    set indent for python-mode

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -7,6 +7,11 @@
 	      (add-to-list 'company-backends 'company-jedi)
 	      (local-set-key (kbd "M-.") #'jedi:goto-definition)
 	      (local-set-key (kbd "C-c C-d") #'jedi:show-doc)))
-  (add-hook 'python-mode-hook 'turn-on-eldoc-mode))
+  (add-hook 'python-mode-hook '(lambda ()
+				 (set (make-local-variable 'python-indent-offset) 2)
+					;(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+					;(turn-on-eldoc-mode)
+				 )
+	    t))
 ; (add-hook 'python-mode-hook 'jedi:setup) ; execute jedi:install-server when first running
 (provide 'init-python)
