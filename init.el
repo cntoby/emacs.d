852d7118509faae466529c91ecd1c9c0a97a1219
Toby <cntoby@gmail.com>
2018-02-26 11:29:09 +0800

    update init.el

--- init.el
+++ init.el
@@ -56,7 +56,7 @@
 ;   "pandoc -f markdown -t html -s -—mathjax -—highlight-style pygments")
  '(package-selected-packages
    (quote
-    (elm-mode elm-yasnippets nodejs-repl company-php yaml-mode window-numbering websocket web-mode sublime-themes rustfmt racer projectile-codesearch powerline org-pandoc org-journal omnisharp material-theme markdown-mode man-commands magit js2-mode jekyll-modes irony-eldoc helm-projectile haskell-snippets go-snippets go-eldoc flycheck-rust flycheck-irony flycheck-haskell exec-path-from-shell erlang dockerfile-mode company-tern company-racer company-lua company-jedi company-irony-c-headers company-irony company-go company-ghci company-ghc cmake-mode))))
+    (kotlin-mode elm-mode elm-yasnippets nodejs-repl company-php yaml-mode window-numbering websocket web-mode sublime-themes rustfmt racer projectile-codesearch powerline org-pandoc org-journal omnisharp material-theme markdown-mode man-commands magit js2-mode jekyll-modes irony-eldoc helm-projectile haskell-snippets go-snippets go-eldoc flycheck-rust flycheck-irony flycheck-haskell exec-path-from-shell erlang dockerfile-mode company-tern company-racer company-lua company-jedi company-irony-c-headers company-irony company-go company-ghci company-ghc cmake-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
