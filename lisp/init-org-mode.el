76c4c9d0e31427e14bec6edf68e36e5224321fea
Toby <cntoby@gmail.com>
2018-03-19 20:12:56 +0800

    upload orgmode config

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -1,5 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(setq org-journal-dir (expand-file-name "~/我的坚果云/Documents/journal/"))
+(setq org-journal-dir (expand-file-name "~/Dropbox/Documents/journal/"))
 (setq org-journal-file-format "%Y%m%d.md")
+(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
 
 (provide 'init-org-mode)
