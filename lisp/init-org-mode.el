bc4545d6308cea1c9f8c3be3aaa2d60eda924dc3
Toby <cntoby@gmail.com>
2018-03-19 23:42:33 +0800

    update org-mode configure

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -2,5 +2,13 @@
 (setq org-journal-dir (expand-file-name "~/Dropbox/Documents/journal/"))
 (setq org-journal-file-format "%Y%m%d.md")
 (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(setq org-agenda-files (list "~/Dropbox/Documents/org/work.org"
+			     "~/Dropbox/Documents/org/home.org"
+			     "~/Dropbox/Documents/org/writing.org"
+			     "~/Dropbox/Documents/org/daily.org"
+			     "~/Dropbox/Documents/org/finance.org"))
 
 (provide 'init-org-mode)
