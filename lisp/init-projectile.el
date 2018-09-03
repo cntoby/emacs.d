ef74c9ad985e2d182aa66bfe1632c498bd46cddf
Toby <cntoby@gmail.com>
2018-09-03 17:30:43 +0800

    set global shortcut for projectile-dired to "C-c p D""

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,6 +1,8 @@
 (when (package-installed-p 'projectile)
   (projectile-global-mode) ; enable all
   (setq projectile-indexing-method 'alien)
+  (global-set-key (kbd "C-c p D") 'projectile-dired)
+
   (if (package-installed-p 'helm-projectile)
       (progn
 	;; (helm-proje ctile)
